849157ac4c102288de993ad6177f1c6b147995b9
kenmueller <kenmueller0@gmail.com>
2021-03-08 03:13:43 -0800

    show image

--- components/Navbar/Profile/index.tsx
+++ components/Navbar/Profile/index.tsx
@@ -1,4 +1,9 @@
+import { Svg } from 'react-optimized-image'
+
 import firebase from 'lib/firebase'
+
+import bell from 'images/bell.svg'
+import down from 'images/chevron-down.svg'
 
 import styles from './index.module.scss'
 
@@ -8,8 +13,20 @@ export interface NavbarProfileProps {
 	user: firebase.User
 }
 
-const NavbarProfile = ({ user }: NavbarProfileProps) => {
-	return <>{user.displayName}</>
-}
+const NavbarProfile = ({ user }: NavbarProfileProps) => (
+	<div className={styles.root}>
+		<button className={styles.notifications}>
+			<Svg src={bell} />
+		</button>
+		<hr className={styles.divider} />
+		<button className={styles.user}>
+			{user.photoURL && (
+				<img className={styles.userImage} src={user.photoURL} />
+			)}
+			{user.displayName ?? 'Anonymous'}
+			<Svg className={styles.userInfo} src={down} />
+		</button>
+	</div>
+)
 
 export default NavbarProfile
--- next.config.js
+++ next.config.js
@@ -1,4 +1,54 @@
 /* eslint-env node */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-module.exports = require('next-optimized-images')()
+const { getCSP, SELF, INLINE, DATA, EVAL } = require('csp-header')
+
+const DEV = process.env.NODE_ENV === 'development'
+
+module.exports = require('next-optimized-images')({
+	headers: () => [
+		{
+			source: '/(.*)',
+			headers: [
+				{
+					key: 'Access-Control-Allow-Origin',
+					value: process.env.NEXT_PUBLIC_ORIGIN
+				},
+				{
+					key: 'Content-Security-Policy',
+					value: getCSP({
+						directives: {
+							'default-src': [SELF],
+							'connect-src': [SELF, 'https://www.googleapis.com'],
+							'font-src': [SELF, 'https://fonts.gstatic.com'],
+							'style-src': [SELF, INLINE, 'https://fonts.googleapis.com'],
+							'script-src': [
+								SELF,
+								...(DEV ? [EVAL] : []),
+								"'sha256-Nqnn8clbgv+5l0PgxcTOldg8mkMKrFn4TvPL+rYUUGg='", // Render-blocking script
+								'https://apis.google.com'
+							],
+							'img-src': [SELF, DATA, 'https://avatars.githubusercontent.com'],
+							'object-src': [SELF, DATA],
+							'frame-src': [
+								SELF,
+								`https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`
+							],
+							'base-uri': SELF,
+							'upgrade-insecure-requests': !DEV
+						}
+					})
+				},
+				{ key: 'Expect-CT', value: '0' },
+				{ key: 'Referrer-Policy', value: 'no-referrer' },
+				{ key: 'Strict-Transport-Security', value: 'max-age=15552000' },
+				{ key: 'X-Content-Type-Options', value: 'nosniff' },
+				{ key: 'X-DNS-Prefetch-Control', value: 'off' },
+				{ key: 'X-Download-Options', value: 'noopen' },
+				{ key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+				{ key: 'X-Permitted-Cross-Domain-Policies', value: 'none' },
+				{ key: 'X-XSS-Protection', value: '0' }
+			]
+		}
+	]
+})
