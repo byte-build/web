a3e3b5e1d3ed290f700d26863e3fad08df19c1d6
kenmueller <kenmueller0@gmail.com>
2021-03-08 00:07:55 -0800

    updated deps

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /* eslint-env node */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-module.exports = require('next-compose-plugins')([
-	[require('next-optimized-classnames')],
-	[require('next-optimized-images')]
-])
+module.exports = require('next-optimized-images')()
