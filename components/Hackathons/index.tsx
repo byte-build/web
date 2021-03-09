49c23a4c9270bf35d5dd9f29c1f78e1e8e91ebf4
kenmueller <kenmueller0@gmail.com>
2021-03-09 04:54:25 -0800

    search field

---
+++ components/Hackathons/index.tsx
@@ -0,0 +1,5 @@
+import { NextPage } from 'next'
+
+const Hackathons: NextPage = () => null
+
+export default Hackathons
--- components/Navbar/Search/index.tsx
+++ components/Navbar/Search/index.tsx
@@ -1,6 +1,6 @@
-import { useCallback } from 'react'
-import { useRecoilValue } from 'recoil'
-import Router from 'next/router'
+import { ChangeEvent, useRef, useCallback, useEffect } from 'react'
+import { useRecoilState } from 'recoil'
+import { useRouter } from 'next/router'
 import { Svg } from 'react-optimized-image'
 
 import queryState from 'state/query'
@@ -10,19 +10,37 @@ import search from 'images/search.svg'
 import styles from './index.module.scss'
 
 const NavbarSearch = () => {
-	const query = useRecoilValue(queryState)
+	const input = useRef<HTMLInputElement | null>(null)
 
-	const showHackathons = useCallback(() => {
-		Router.push('/hackathons')
-	}, [])
+	const router = useRouter()
+	const [query, setQuery] = useRecoilState(queryState)
+
+	const active = router.asPath === '/hackathons'
+
+	const push = useCallback(() => {
+		router.push('/hackathons')
+	}, [router])
+
+	const onChange = useCallback(
+		(event: ChangeEvent<HTMLInputElement>) => {
+			setQuery(event.target.value)
+		},
+		[setQuery]
+	)
+
+	useEffect(() => {
+		if (active) input.current?.focus()
+	}, [active, input])
 
 	return (
-		<div className={styles.root} onClick={showHackathons}>
+		<div className={styles.root} onClick={active ? undefined : push}>
 			<input
+				ref={input}
 				className={styles.input}
 				placeholder="Hackathons"
 				value={query}
-				readOnly
+				onChange={active ? onChange : undefined}
+				readOnly={!active}
 			/>
 			<Svg className={styles.icon} src={search} />
 		</div>
