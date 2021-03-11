99361c3a29a86254c818ca5ed8e309d6c2c124da
kenmueller <kenmueller0@gmail.com>
2021-03-11 05:17:21 -0800

    cache stripe

--- lib/stripe/load.ts
+++ lib/stripe/load.ts
@@ -1,6 +1,8 @@
 import { loadStripe as load } from '@stripe/stripe-js/pure'
 
+let stripe: import('@stripe/stripe-js').Stripe | null = null
+
 const key = process.env.NEXT_PUBLIC_STRIPE_API_KEY as string
-const loadStripe = () => load(key)
+const loadStripe = async () => (stripe ??= await load(key))
 
 export default loadStripe
