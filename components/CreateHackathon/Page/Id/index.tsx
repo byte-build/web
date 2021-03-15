db83e4a35e8d2559e9f36b13561b73f4bcb10b9e
kenmueller <kenmueller0@gmail.com>
2021-03-15 02:07:10 -0700

    redesigned id

--- components/CreateHackathon/Page/Id/index.tsx
+++ components/CreateHackathon/Page/Id/index.tsx
@@ -43,9 +43,23 @@ const isTaken = throttle(
 
 const CreateHackathonId = () => {
 	const [{ id: state }, setState] = useRecoilState(createHackathonState)
+
+	const [isFocused, setIsFocused] = useState(false)
 	const [isLoading, setIsLoading] = useState(false)
 
 	const { value, taken } = state
+
+	const onInput = useCallback((input: HTMLInputElement | null) => {
+		input?.focus()
+	}, [])
+
+	const onFocus = useCallback(() => {
+		setIsFocused(true)
+	}, [setIsFocused])
+
+	const onBlur = useCallback(() => {
+		setIsFocused(false)
+	}, [setIsFocused])
 
 	const onChange = useCallback(
 		(event: ChangeEvent<HTMLInputElement>) => {
@@ -65,7 +79,7 @@ const CreateHackathonId = () => {
 	}, [value, setState, setIsLoading])
 
 	return (
-		<div className={styles.root} aria-invalid={value ? taken : undefined}>
+		<div className={styles.root}>
 			<div className={styles.header}>
 				<h4 className={styles.title}>Link</h4>
 				{value &&
@@ -79,16 +93,19 @@ const CreateHackathonId = () => {
 						<span className={styles.available}>available</span>
 					))}
 			</div>
-			<div className={styles.value}>
+			<div className={styles.value} data-focused={isFocused}>
 				<label className={styles.base} htmlFor="create-hackathon-id">
 					{process.env.NEXT_PUBLIC_ORIGIN}/
 				</label>
 				<input
-					className={styles.input}
+					ref={onInput}
 					id="create-hackathon-id"
+					className={styles.input}
 					placeholder="my-hackathon"
 					value={value}
 					onChange={onChange}
+					onFocus={onFocus}
+					onBlur={onBlur}
 				/>
 			</div>
 		</div>
